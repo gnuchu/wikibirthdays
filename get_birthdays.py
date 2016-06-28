1bd37032bc056377d11a07d2c2998707c4ec8e54
John Griffiths <john.griffiths@aberdeen-asset.com>
2016-06-28 10:44:15 +0100

    add picture dl

--- get_birthdays.py
+++ get_birthdays.py
@@ -6,9 +6,28 @@ import urllib.request
 from datetime import date
 from datetime import timedelta
 import sys
+import traceback
+
+def get_image_url(page_title):
+  placeholder = "https://placeholdit.imgix.net/~text?txtsize=33&txt=Pic&w=120&h=120"
+  resp2=""
+
+  try:
+    url2 = "https://en.wikipedia.org/wiki/" + page_title
+    doc2 = urllib.request.urlopen(url2).read()
+    soup2 = BeautifulSoup(doc2, "html.parser")
+    a2 = soup2.find('a', {'class' : 'image'})
+    resp2 = 'https:' + a2.find_next('img')['src']
+  except NameError:
+    print(traceback.format_exc())
+    sys.exit()
+  except:
+    resp2 = placeholder
+
+  return resp2
+
 
 def main():
-  base_url = 'https://en.wikipedia.org'
   f = open("birthdays.html", "w", encoding="utf-8")
 
   header = '''
@@ -17,16 +36,30 @@ def main():
     <head>
       <meta charset="utf-8">
       <title>Birthdays - Last week and this.</title>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.js"></script>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.css">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.lazyload/1.9.1/jquery.lazyload.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css">
+      <style>
+        img {
+          width: auto;
+          height: 200px;
+          max-width: 200px;
+        }
+      </style>
     </head>
     <body>
+      <script>
+        $(function() {
+          $("img.lazy").lazyload();
+        });
+      </script>
       <div class='container'>
         <table class="table table-bordered table-condensed">
           <thead>
             <tr>
               <th>Name</th>
+              <th>Image</th>
             </tr>
           </thead>
           <tbody>
@@ -48,7 +81,6 @@ def main():
   tr_e = '</tr>'
   td_s = '<td>'
   td_e = '</td>'
-
 
   while start_date < end_date:
     month = start_date.strftime('%B')
@@ -81,32 +113,15 @@ def main():
         except:
           page_title = page_title.replace('https://en.wikipedia.org/wiki/',"")
 
-      #page_title = page_title.replace('/wiki/',"")
-      #image_link = get_image_url(page_title)
+      page_title = page_title.replace('/wiki/',"")
+      image_link = get_image_url(page_title)
       f.write('<tr>')
       f.write(row+"\n")
-      #f.write('<td>' + image_link + '</td>')
+      f.write('<td><img class="lazy" data-original="' + image_link + '"></img></td>')
       f.write('</tr>')
 
   f.write(footer+"\n")
   f.close()
 
-def get_image_url(page_title):
-  placeholder = "https://placeholdit.imgix.net/~text?txtsize=33&txt=ImageN/A&w=200&h=200"
-  api_base = 'http://en.wikipedia.org/w/api.php?action=query&titles=PAGE_TITLE&prop=pageimages&format=json'
-  #url = urllib.urlencode(api_base.replace('PAGE_TITLE', page_title))
-  url = api_base.replace('PAGE_TITLE', page_title)
-  
-  resp={}
-  try:
-    resp = urllib.request.urlopen(url).read().decode('utf-8')
-  except:
-    resp = placeholder
-
-  return resp
-
 if __name__ == "__main__":
-    main()
-
-
-
+  main()
